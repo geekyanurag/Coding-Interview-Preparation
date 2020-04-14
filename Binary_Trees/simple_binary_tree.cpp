8ee6cb62e2ad4eb0ab8450d8d0ac26dd0fb50477
AnuragSaw <anurag.kumar@btech.christuniversity.in>
2020-04-15 00:52:54 +0530

    added the insert and print functions

--- Binary_Trees/simple_binary_tree.cpp
+++ Binary_Trees/simple_binary_tree.cpp
@@ -6,10 +6,26 @@ struct Node{
     Node* left, *right;
 };
 
-void insert(int data){
+Node* insert(int data){
+    Node* temp = new Node();
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
 
+void print(Node* head){
+    if(head == NULL)
+        return;
+    print(head->left);
+    cout<<head->data<<" ";
+    print(head->right);
 }
 
 int main(){
-    
+    Node* head = insert(1);
+    head->left = insert(2);
+    head->right = insert(3);
+    head->left->left = insert(4);
+    print(head);
+    return 0;
 }
