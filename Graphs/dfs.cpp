5f51f788cc7f9113e5a8e97410dd42dd62d3f15d
AnuragSaw <anurag.kumar@btech.christuniversity.in>
2020-04-15 01:04:06 +0530

    dfs using stl

---
+++ Graphs/dfs.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int maxn = 1e5;
+vector<int>g[maxn];
+int vis[maxn];
+
+void dfs(int u){
+    vis[u] = 1;
+    cout<<u<<" ";
+    for(int i : g[u]){
+        if(!vis[i])
+            dfs(i);
+    }
+}
+
+int main(){
+    int v, n;
+    cin>>v>>n;
+    memset(vis, 0, sizeof(vis));
+    for(int i =0; i<n; i++){
+        int x, y;
+        cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    dfs(0);
+}
