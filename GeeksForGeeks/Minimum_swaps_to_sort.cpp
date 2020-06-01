a00f453e981087c86d0defd045f8eac399e3ef7a
AnuragSaw <34518493+geekyanurag@users.noreply.github.com>
2020-06-01 14:01:34 +0530

    adding a new method

--- GeeksForGeeks/Minimum_swaps_to_sort.cpp
+++ GeeksForGeeks/Minimum_swaps_to_sort.cpp
@@ -17,3 +17,30 @@ int minSwaps(int A[], int N){
     }
     return count;
 }
+
+// Efficient approach is to use the graph cycle technique.
+// In this technique we take the elements which are being swapped , if 2 no's are to be swapped then the min no. of swapping is 2 - 1 = 1,
+// so if n is the number of elements then the min swaps is n - 1. So ans = summation of no. of cycles * (sizeofcycle - 1);
+int minSwaps(int A[], int N){
+    pair<int,int> arr[N];
+    for(int i =0; i<N; i++){
+        arr[i].first = A[i];
+        arr[i].second = i;
+    }
+    sort(arr, arr + N);
+    vector<bool>vis(N , false);
+    int ans = 0;
+    for(int i = 0; i<N; i++){
+        if(vis[i] || arr[i].second == i)
+            continue;
+        int j = i, size = 0;
+        while(!vis[j]){
+            vis[j] = 1;
+            j = arr[j].second;
+            size++;
+        }
+        if(size > 0)
+            ans += size - 1;
+    }
+    return ans;
+}
