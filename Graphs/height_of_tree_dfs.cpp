a16c9460260b7ff06041c4511cd79162022c0ca0
AnuragSaw <34518493+geekyanurag@users.noreply.github.com>
2020-07-31 06:40:59 +0530

    adding comments

--- Graphs/height_of_tree_dfs.cpp
+++ Graphs/height_of_tree_dfs.cpp
@@ -1,33 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Calculating height of a general tree.
 const int maxn = 1e5;
 
-void dfs(vector<int>g[], vector<int>&vis, int u, int height[]){
-    vis[u] = 1;
+vector<int>g[maxn];
+int height[maxn];
+
+void dfs(int u, int p){
     height[u] = 0;
-    cout<<u<<" ";
     for(auto i : g[u]){
-        if(!vis[i]){
-            dfs(g, vis, i, height);
-        }
-        height[u] = max(height[u], 1+height[i]);
+        if(i == p) continue;
+        dfs(i, u);
+        height[u] = max(height[u], 1+height[i]);    // height of the parent will be max (height of the tree , 1 + (height of its childrens) ).
     }
 }
 int main(){
     int v, n;
     cin>>v>>n;
-    vector<int>g[v];
-    vector<int>vis(v, 0);
-    int height[v];
     for(int i =0; i<n; i++){
         int x, y;
         cin>>x>>y;
         g[x].push_back(y);
-        //g[y].push_back(x);
+        g[y].push_back(x);
     }
-    dfs(g, vis, 0, height);
-    for(int i =0; i<v; i++){
-        cout<<i<<" "<<height[i]<<endl;
-    }
+    dfs(0, -1);
+    cout<<height[0]<<endl;  // Height of the root.
 }
