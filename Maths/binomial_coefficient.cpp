a14bb3bd1d1592b33e9a0cdaea7ca15c891aef42
AnuragSaw <34518493+geekyanurag@users.noreply.github.com>
2020-10-13 20:10:56 +0530

    added more methods

--- Maths/binomial_coefficient.cpp
+++ Maths/binomial_coefficient.cpp
@@ -25,6 +25,11 @@ int binomial(int n, int k){
     int c[n + 1][k + 1];
 
     for(int i = 0; i<=n; i++){
+        for(int j = 0; j<=k; j++)
+            c[i][j] = 0;
+    }
+    
+    for(int i = 0; i<=n; i++){
         for(int j = 0; j<=min(i, k); j++){
             if(j == 0 || j == i) c[i][j] = 1;
             else
